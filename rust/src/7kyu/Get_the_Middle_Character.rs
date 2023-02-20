594c85cf148693e7d447930871713c78d0ef1065
matt.graham <matt.graham@team.aussiebroadband.com.au>
2023-02-21 10:02:23 +1100

    codewars 21/02/2023

--- rust/src/7kyu/Get_the_Middle_Character.rs
+++ rust/src/7kyu/Get_the_Middle_Character.rs
@@ -1,6 +1,6 @@
 fn get_middle(s:&str) -> &str {
     if s.len() % 2 == 0 {
-        &s[(s.len() / 2)..(s.len() / 2 + 2)]
+        &s[(s.len() / 2 - 1)..(s.len() / 2 + 1)]
     } else {
         &s[(s.len() / 2)..(s.len() / 2 + 1)]
     }
