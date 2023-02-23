6fd31fcbee6109301454d2f6da4c3f75be7ee153
matt.graham <matt.graham@team.aussiebroadband.com.au>
2023-02-23 11:19:28 +1100

    codewars 23022023

--- python/7kyu/Mumbling.py
+++ python/7kyu/Mumbling.py
@@ -2,12 +2,9 @@ def accum(s):
     string_list = list(s.lower())
     result = []
     for i in range(len(string_list)):
-        if el == 0:
+        if i == 0:
             result.append(string_list[i].upper())
         else:
             result.append(string_list[i].upper() + (string_list[i] *  i))
     
     return "-".join(result)
-
-Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnn-Uuuuuuuuuuu
-Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu
